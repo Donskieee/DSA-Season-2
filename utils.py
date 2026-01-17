fd734c6ffc9dcab8beeaef4497887cc35f8e135e
Donskieee <noriestadon@gmail.com>
2026-01-17 10:15:47 +0800

    Refactor is_valid_date function
    Refactored is_valid_date function to improve clarity and added leap year support.

--- utils.py
+++ utils.py
@@ -1,32 +1,36 @@
-# utils.py
+#utils.py
 
 def is_valid_date(date_str):
     
-    # Check basic format
+    #checks if string yung input
     if not isinstance(date_str, str):
         return False
-
+    
+    #if string si input it get split in the "-" part
     parts = date_str.split("-")
     if len(parts) != 2:
         return False
-
+    
     month_str, day_str = parts
 
-    # Check if both parts are numeric
+
+
+    #checks if month and day string are digits
     if not month_str.isdigit() or not day_str.isdigit():
         return False
 
+    #converts month and day from string to integers
     month = int(month_str)
     day = int(day_str)
 
-    
-    if month < 1 or month > 12:
+    if month  > 12 or month < 1:
         return False
+    
+    #also works for leap years
 
-    #non leap year
     days_in_month = {
-        1: 31,  
-        2: 28,  
+        1: 31,
+        2: 29,
         3: 31,
         4: 30,
         5: 31,
@@ -36,13 +40,11 @@ def is_valid_date(date_str):
         9: 30,
         10: 31,
         11: 30,
-        12: 31
-    }
+        12: 31,
 
-    
+    }
+    #checks if day is less than 1 or more than day_in_month
     if day < 1 or day > days_in_month[month]:
         return False
-
-
+    
     return True
-
